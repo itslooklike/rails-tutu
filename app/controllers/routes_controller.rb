0552421d4efa0e5b8d706aa423258d670cf2a868
itslooklike <delsol@bk.ru>
2019-04-28 18:18:58 +0300

    route

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,34 @@
 class RoutesController < ApplicationController
+  before_action :set_route, only: [:show, :edit, :update, :destroy]
+
   def index
     @routes = Route.all
+  end
+
+  def new
+    @route = Route.new
+  end
+
+  def show
+  end
+
+  def create
+    @route = Route.new(route_params)
+
+    if @route.save
+      redirect_to @route, notice: 'Railway station was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_route
+    @route = Route.find(params[:id])
+  end
+
+  def route_params
+    params.require(:route).permit(:name)
   end
 end
